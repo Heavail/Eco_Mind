5c4daf6e2b5fb9e04d90cdc39db9cb74056954b1
Heavail <satyamkr209@gmail.com>
2026-01-25 21:59:50 +0530

    Remove MySQL database settings from settings.py
    Removed database configuration settings for MySQL.

--- Main/Main/settings.py
+++ Main/Main/settings.py
@@ -77,18 +77,6 @@ WSGI_APPLICATION = 'Main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ecomind',
-        'USER': 'root',
-        'PASSWORD': 'Password123',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
